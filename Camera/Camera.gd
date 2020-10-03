7a66d6116bb66f3ef773f81cff0aff70f11e44c9
Mark Ribaudo <maribaud@iu.edu>
2020-10-03 16:14:59 -0400

    A juicier brick breaker

--- Camera/Camera.gd
+++ Camera/Camera.gd
@@ -1,5 +1,38 @@
 extends Camera2D
 
 
+export var decay = [0,1]                   
+export var max_offset = Vector2(100, 50)    
+export var max_roll = 0.1                   
+export (NodePath) var target                # Assign the node this camera will follow.
+
+var trauma = [0,2]                          
+var trauma_power = [2,3]                    
+onready var noise = OpenSimplexNoise.new()
+var noise_y = 0
+
 func _ready():
-	pass
+  randomize()
+  noise.seed = randi()
+  noise.period = 4
+  noise.octaves = 2
+
+func _process(delta):
+	if target:
+		global_position = get_node(target).global_position
+	if trauma:
+		trauma = max(trauma - decay * delta, 0)
+		shake()
+
+
+
+
+func shake():
+  var amount = pow(trauma, trauma_power)
+  noise_y += 1
+  rotation = max_roll * amount * noise.get_noise_2d(noise.seed, noise_y)
+  offset.x = max_offset.x * amount * noise.get_noise_2d(noise.seed*2, noise_y)
+  offset.y = max_offset.y * amount * noise.get_noise_2d(noise.seed*3, noise_y)
+  
+func add_trauma(amount):
+  trauma = min(trauma + amount, 1.0)
